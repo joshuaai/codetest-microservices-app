d6b75602a74420832b87efbf866cbfff28834bf5
joshuaai <joshuaiheji@gmail.com>
2020-01-17 14:26:27 +0100

    setting up flask bluprints using application factories pattern

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,9 +1,11 @@
 import unittest
 
 from flask.cli import FlaskGroup
-from project import app, db
 
-cli = FlaskGroup(app)
+from project import create_app, db
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 
 @cli.command()
